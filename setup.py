56bb51218abc78a726e9f0bc65523dfb887c14a8
Zachary Juang <zachary822@me.com>
2021-01-24 11:33:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mfrc522",
-    version="0.0.8",
-    author="Pi My Life Up",
-    author_email="support@pimylifeup.com",
+    version="0.0.9",
+    author="zachary822",
     description="A library to integrate the MFRC522 RFID readers with the Raspberry Pi",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pimylifeup/MFRC522-python",
+    url="https://github.com/zachary822/MFRC522-python",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'RPi.GPIO',
-        'spidev'
-        ],
+    install_requires=["RPi.GPIO", "spidev"],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
-        'Topic :: System :: Hardware',
+        "Topic :: System :: Hardware",
     ],
 )
