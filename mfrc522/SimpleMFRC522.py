cdba06cb213c15685748795e9fadbaeec3f92fb3
Ralf Herzog <ralf@rherzog.de>
2021-10-06 14:28:09 +0200

    Updated SimpleMFRC522: Renamed variables + optional block_addresses

--- mfrc522/SimpleMFRC522.py
+++ mfrc522/SimpleMFRC522.py
@@ -5,14 +5,12 @@ from .exceptions import MFRC522Exception
 
 
 class SimpleMFRC522:
-    KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-    BLOCK_ADDRS = [8, 9, 10]
-
-    def __init__(self, pin_rst, key=None):
+    def __init__(self, pin_rst, key=None, block_addresses=None):
         self.reader = MFRC522(pin_rst)
 
-        if key is not None:
-            self.KEY = key
+        self.key = key or [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+        # self.block_addresses = block_addresses or [8, 9, 10]
+        self.block_addresses = block_addresses
 
     def __enter__(self):
         return self
@@ -33,27 +31,27 @@ class SimpleMFRC522:
                 return hid
 
     def read_id_no_block(self):
-        (status, TagType) = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
+        status, tag_type = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
         if status != self.reader.MI_OK:
             return None
-        (status, uid) = self.reader.mfrc522_anticoll()
+        status, uid = self.reader.mfrc522_anticoll()
         if status != self.reader.MI_OK:
             return None
         return self.uid_to_hex(uid)
 
     def read_no_block(self):
-        (status, TagType) = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
+        status, tag_type = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
         if status != self.reader.MI_OK:
             return None, None
-        (status, uid) = self.reader.mfrc522_anticoll()
+        status, uid = self.reader.mfrc522_anticoll()
         if status != self.reader.MI_OK:
             return None, None
         self.reader.mfrc522_select_tag(uid)
-        status = self.reader.mfrc522_auth(self.reader.PICC_AUTHENT1A, 11, self.KEY, uid)
+        status = self.reader.mfrc522_auth(self.reader.PICC_AUTHENT1A, 11, self.key, uid)
         data = []
         text_read = ""
-        if status == self.reader.MI_OK:
-            for block_num in self.BLOCK_ADDRS:
+        if status == self.reader.MI_OK and self.block_addresses is not None:
+            for block_num in self.block_addresses:
                 try:
                     data += self.reader.mfrc522_read(block_num)
                 except MFRC522Exception:
@@ -70,30 +68,32 @@ class SimpleMFRC522:
                 return hid, text_in
 
     def write_no_block(self, text):
-        (status, TagType) = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
+        if self.block_addresses is None:
+            raise MFRC522Exception("cannot write without block_addresses")
+        status, tag_type = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
         if status != self.reader.MI_OK:
             return None, None
-        (status, uid) = self.reader.mfrc522_anticoll()
+        status, uid = self.reader.mfrc522_anticoll()
         if status != self.reader.MI_OK:
             return None, None
         self.reader.mfrc522_select_tag(uid)
-        status = self.reader.mfrc522_auth(self.reader.PICC_AUTHENT1A, 11, self.KEY, uid)
+        status = self.reader.mfrc522_auth(self.reader.PICC_AUTHENT1A, 11, self.key, uid)
         if status == self.reader.MI_OK:
-            data = bytearray(text.ljust(len(self.BLOCK_ADDRS) * 16).encode())
-            for i, block_num in enumerate(self.BLOCK_ADDRS):
+            data = bytearray(text.ljust(len(self.block_addresses) * 16).encode())
+            for i, block_num in enumerate(self.block_addresses):
                 self.reader.mfrc522_write(block_num, data[(i * 16): (i + 1) * 16])
         self.reader.mfrc522_stop_crypto1()
-        return self.uid_to_hex(uid), text[0 : (len(self.BLOCK_ADDRS) * 16)]
+        return self.uid_to_hex(uid), text[0: (len(self.block_addresses) * 16)]
 
     def dump_no_block(self):
-        (status, TagType) = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
+        status, tag_type = self.reader.mfrc522_request(self.reader.PICC_REQIDL)
         if status != self.reader.MI_OK:
             return None, None
-        (status, uid) = self.reader.mfrc522_anticoll()
+        status, uid = self.reader.mfrc522_anticoll()
         if status != self.reader.MI_OK:
             return None, None
         self.reader.mfrc522_select_tag(uid)
-        data = self.reader.mfrc522_dump_classic1k([self.KEY] * 32, uid)
+        data = self.reader.mfrc522_dump_classic1k([self.key] * 32, uid)
         self.reader.mfrc522_stop_crypto1()
         assert len(data) <= 1024
         return self.uid_to_hex(uid), data
